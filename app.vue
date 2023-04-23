9e41c2baa450872a361114adda37b2778664a642
Tom <tom.barreau1994@gmail.com>
2023-04-23 13:31:16 +0200

    modiftest

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <!-- <NuxtWelcome /> -->
-    <h1 class="text-5xl text-center p-5">Welcome</h1>
+    <h1 class="text-5xl text-center p-5">Welcomesrdgzrghtr</h1>
   </div>
 </template>
